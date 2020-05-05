19e9e328b1fcdf7fd961cb1079feac33011daa4b
David <cortessuarezdavid@gmail.com>
2020-05-05 19:16:41 +0200

    Rename Student

--- app/src/main/java/cat/udl/tidic/amb/easytutor/models/Student.java
+++ app/src/main/java/cat/udl/tidic/amb/easytutor/models/Student.java
@@ -2,21 +2,21 @@ package cat.udl.tidic.amb.easytutor.models;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Alumne extends User {
+public class Student extends User {
     @SerializedName("materia")
     private String subject;
 
-    public Alumne(String subject) {
+    public Student(String subject) {
         super();
         this.subject = subject;
     }
 
-    public Alumne(String name, String surname, String username, String email, String gender, String phone, String subject) {
+    public Student(String name, String surname, String username, String email, String gender, String phone, String subject) {
         super(name, surname, username, email, gender, phone);
         this.subject = subject;
     }
 
-    public Alumne(String name, String surname, String username, String email, String gender, String phone, String photo, String zone, String subject) {
+    public Student(String name, String surname, String username, String email, String gender, String phone, String photo, String zone, String subject) {
         super(name, surname, username, email, gender, phone, photo, zone);
         this.subject = subject;
     }
